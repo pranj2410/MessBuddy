305c3226e7e56f0c148798350af22aa74df47214
pranj2410 <96702695+pranj2410@users.noreply.github.com>
2022-04-22 23:53:06 +0530

    First Page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './messlogo.jpg';
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Project Start</h1>
+      <img src={logo} className="logo"/>
+      <h1 className="MessBuddy">Mess Buddy</h1>
+      
+
+      <form className="InputContainer">
+        <input className="InputField" placeholder='First Name'></input>
+        <input className="InputField" placeholder='Last Name'></input>
+        <input className="InputField" placeholder='Your Email'></input>
+        <input type ="password" className="InputField" placeholder='Password'></input>
+        <button className="RegisterNow">Register Now</button>
+      </form>
+      <p className = "Message">Already have an account?<span className="login">Log in</span></p>
     </div>
   );
 }

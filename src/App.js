e0cdfd308a58ec2a4425501aee9e523b078aedd3
pa859 <upadhyaypavan72@gmail.com>
2022-04-23 18:57:58 +0530

    adding second page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
+import Register from './components/Register';
+import LogIn from './components/LogIn';
 import './App.css';
-import logo from './messlogo.jpg';
-
 
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="logo"/>
-      <h1 className="MessBuddy">Mess Buddy</h1>
-      
-
-      <form className="InputContainer">
-        <input className="InputField" placeholder='First Name'></input>
-        <input className="InputField" placeholder='Last Name'></input>
-        <input className="InputField" placeholder='Your Email'></input>
-        <input type ="password" className="InputField" placeholder='Password'></input>
-        <button className="RegisterNow">Register Now</button>
-      </form>
-      <p className = "Message">Already have an account?<span className="login">Log in</span></p>
+      <LogIn/>
     </div>
   );
 }
